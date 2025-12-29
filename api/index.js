1630251a6aba894ab85e3216d191381d6220766e
SIFAT <sifatsararchistee@gmail.com>
2025-12-29 23:04:37 +0600

    forgot to save the file

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: true,  // This allows all origins temporarily for testing
+  origin: true,  
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
